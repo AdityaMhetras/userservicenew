5d535d540b877555e8867c6e7c66725e9ac68ffc
aditya <mhetrasaditya@gmail.com>
2025-10-21 22:57:12 +0530

    use traditional dtos

--- src/main/java/dev/naman/userservicetestfinal/services/AuthService.java
+++ src/main/java/dev/naman/userservicetestfinal/services/AuthService.java
@@ -6,24 +6,20 @@ import dev.naman.userservicetestfinal.models.SessionStatus;
 import dev.naman.userservicetestfinal.models.User;
 import dev.naman.userservicetestfinal.repositories.SessionRepository;
 import dev.naman.userservicetestfinal.repositories.UserRepository;
-import lombok.Setter;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.util.MultiValueMap;
 import org.springframework.util.MultiValueMapAdapter;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 
 @Service
 public class AuthService {
-    private UserRepository userRepository;
-    private SessionRepository sessionRepository;
+    private final UserRepository userRepository;
+    private final SessionRepository sessionRepository;
 
     public AuthService(UserRepository userRepository, SessionRepository sessionRepository) {
         this.userRepository = userRepository;
@@ -34,7 +30,7 @@ public class AuthService {
         Optional<User> userOptional = userRepository.findByEmail(email);
 
         if (userOptional.isEmpty()) {
-            return null;
+            return new ResponseEntity<>(null, null, HttpStatus.NOT_FOUND);
         }
 
         User user = userOptional.get();
@@ -51,7 +47,7 @@ public class AuthService {
         session.setUser(user);
         sessionRepository.save(session);
 
-        UserDto userDto = new UserDto();
+        UserDto userDto = UserDto.from(user);
 
 //        Map<String, String> headers = new HashMap<>();
 //        headers.put(HttpHeaders.SET_COOKIE, token);
@@ -60,11 +56,9 @@ public class AuthService {
         headers.add(HttpHeaders.SET_COOKIE, "auth-token:" + token);
 
 
+        //        response.getHeaders().add(HttpHeaders.SET_COOKIE, token);
 
-        ResponseEntity<UserDto> response = new ResponseEntity<>(userDto, headers, HttpStatus.OK);
-//        response.getHeaders().add(HttpHeaders.SET_COOKIE, token);
-
-        return response;
+        return new ResponseEntity<>(userDto, headers, HttpStatus.OK);
     }
 
     public ResponseEntity<Void> logout(String token, Long userId) {
