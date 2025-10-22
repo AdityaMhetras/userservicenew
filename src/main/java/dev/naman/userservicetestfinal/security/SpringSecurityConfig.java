fe8393c35a897b7ffbe2e608f7acd2492891d259
aditya <mhetrasaditya@gmail.com>
2025-10-22 16:20:27 +0530

    spring security fix for unauthorized 401

---
+++ src/main/java/dev/naman/userservicetestfinal/security/SpringSecurityConfig.java
@@ -0,0 +1,38 @@
+package dev.naman.userservicetestfinal.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+//    @Order(1)
+    public SecurityFilterChain filteringCriteria(HttpSecurity http) throws Exception {
+/*        http.cors().disable();
+        http.csrf().disable();
+        http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+//        http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+//        http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/auth/*").authenticated());
+        return http.build();*/
+
+        return http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                                .anyRequest().permitAll()
+                        // .requestMatchers("/auth/*").authenticated() // Uncomment to restrict
+                )
+                .build();
+
+/*        .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers("/auth/**").authenticated()
+                .anyRequest().permitAll()
+        );*/
+
+    }
+}
