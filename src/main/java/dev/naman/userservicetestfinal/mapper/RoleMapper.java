82646a4e37e67bf23d7f39b69ed9124648615c6d
aditya <mhetrasaditya@gmail.com>
2025-10-22 16:03:40 +0530

    in annotationProcessorPaths, we have first lombok, then its dependency like mapstruct

--- src/main/java/dev/naman/userservicetestfinal/mapper/RoleMapper.java
+++ src/main/java/dev/naman/userservicetestfinal/mapper/RoleMapper.java
@@ -3,12 +3,17 @@ package dev.naman.userservicetestfinal.mapper;
 import dev.naman.userservicetestfinal.dtos.RoleDto;
 import dev.naman.userservicetestfinal.models.Role;
 import org.mapstruct.*;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface RoleMapper {
+
+    RoleMapper INSTANCE = Mappers.getMapper(RoleMapper.class);
     Role toEntity(RoleDto roleDto);
 
     RoleDto toDto(Role role);
+
+    String map(Role value);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     Role partialUpdate(RoleDto roleDto, @MappingTarget Role role);
