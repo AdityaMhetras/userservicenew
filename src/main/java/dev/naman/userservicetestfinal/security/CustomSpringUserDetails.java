c0aa1b6dcff0f7a867ca1887cc3d01479e2df7ba
aditya <mhetrasaditya@gmail.com>
2025-10-23 19:10:44 +0530

    convert to record

--- src/main/java/dev/naman/userservicetestfinal/security/CustomSpringUserDetails.java
+++ src/main/java/dev/naman/userservicetestfinal/security/CustomSpringUserDetails.java
@@ -7,13 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
-public class CustomSpringUserDetails implements UserDetails {
-
-    private final User user;
-
-    public CustomSpringUserDetails(User user) {
-        this.user = user;
-    }
+public record CustomSpringUserDetails(User user) implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
