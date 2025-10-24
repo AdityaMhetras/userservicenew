86efcdd7bb2162054820dc713a6b2535db6204ec
aditya <mhetrasaditya@gmail.com>
2025-10-25 00:30:55 +0530

    save RegisteredClient object in db

---
+++ src/test/java/dev/naman/userservicetestfinal/ClientSaveTest.java
@@ -0,0 +1,45 @@
+package dev.naman.userservicetestfinal;
+
+import dev.naman.userservicetestfinal.security.repositories.JpaRegisteredClientRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.test.annotation.Commit;
+
+import java.util.UUID;
+
+@SpringBootTest
+public class ClientSaveTest {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private JpaRegisteredClientRepository jpaRegisteredClientRepository;
+
+    @Test
+    @Commit
+    public void saveClient() {
+        RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("productservice")
+                .clientSecret(bCryptPasswordEncoder.encode("secret"))
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .redirectUri("http://127.0.0.1:9000/login/oauth2/code/oidc-client")
+                .postLogoutRedirectUri("http://127.0.0.1:9000/")
+                .scope(OidcScopes.OPENID)
+                .scope(OidcScopes.PROFILE)
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                .build();
+
+        jpaRegisteredClientRepository.save(oidcClient);
+    }
+}
